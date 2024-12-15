ebcba0693ec6065875d887b35fa045dcc921da41
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-12-15 13:38:53 +0300

    .

---
+++ Week02/15.12.2024/Proje14_EFCore/EfCoreOneTwoOne - Kopya/EfCore.Entity/Concrete/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using EfCore.Entity.Abstract;
+
+namespace EfCore.Entity.Concrete;
+
+public class Category : BaseEntity
+{
+    public string? Description { get; set; }
+
+    public IEnumerable<Product> Products { get; set; }=[];
+
+
+}
