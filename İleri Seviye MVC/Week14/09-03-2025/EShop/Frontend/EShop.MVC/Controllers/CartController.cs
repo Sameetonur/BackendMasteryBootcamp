56d39b5ce0a405b0b4f8287366368146a50cbae9
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2025-03-12 23:40:15 +0300

    .

--- İleri Seviye MVC/Week14/09-03-2025/EShop/Frontend/EShop.MVC/Controllers/CartController.cs
+++ İleri Seviye MVC/Week14/09-03-2025/EShop/Frontend/EShop.MVC/Controllers/CartController.cs
@@ -5,13 +5,13 @@ using NToastNotify;
 
 namespace EShop.MVC.Controllers
 {
-   [Authorize]
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ICartService _cartService;
         private readonly IToastNotification _toastNotification;
 
-    
+
         public CartController(ICartService cartService, IToastNotification toastNotification)
         {
             _cartService = cartService;
@@ -25,35 +25,35 @@ namespace EShop.MVC.Controllers
             if (!response.IsSuccessful)
             {
                 _toastNotification.AddErrorToastMessage(response.Error ?? "Sepet bilgisi alınamadı!");
-                return RedirectToAction("Index","Homde");
-                
+                return RedirectToAction("Index", "Homde");
+
             }
             return View(response.Data);
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddToCart(int productId,int quantity)
+        public async Task<IActionResult> AddToCart(int productId, int quantity)
         {
-            if (quantity<1)
+            if (quantity < 1)
             {
                 _toastNotification.AddErrorToastMessage("Geçersiz ürün miktarı!");
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
             }
-            var response = await _cartService.AddToCartAsync(productId,quantity);
+            var response = await _cartService.AddToCartAsync(productId, quantity);
 
             return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
 
-        public async Task<IActionResult> ChangeQuantity(int cartItemId , int quantity)
+        public async Task<IActionResult> ChangeQuantity(int cartItemId, int quantity)
         {
             if (quantity < 1)
             {
                 _toastNotification.AddErrorToastMessage("Geçersiz ürün miktarı!");
                 return RedirectToAction(nameof(Index));
             }
-            var response = await _cartService.ChangeQuantityAsync(cartItemId,quantity);
+            var response = await _cartService.ChangeQuantityAsync(cartItemId, quantity);
             return RedirectToAction(nameof(Index));
 
         }
