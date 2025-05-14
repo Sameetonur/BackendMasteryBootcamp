d16f92c1bfb35f4a808431f05c5bbe89b7ca640e
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2025-05-14 18:13:12 +0300

    .

--- 03-API/Week05/04-01-2025/EShop/EShop.Service/obj/Debug/net8.0/EShop.Service.AssemblyInfo.cs
+++ 03-API/Week05/04-01-2025/EShop/EShop.Service/obj/Debug/net8.0/EShop.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EShop.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd31bbcbaf3eec6bdcb9ebb5ef2b84849e58ffc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618a20932b2adc6f0b39c51bd25a7c9d534fda62")]
 [assembly: System.Reflection.AssemblyProductAttribute("EShop.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EShop.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
